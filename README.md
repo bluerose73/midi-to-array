c46ee698ff9aff78322b8d932f0718eac0e806a7
bluerose73 <2544747588@qq.com>
2022-05-07 16:04:39 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # midi-to-array
-Convert midi to 2 arrays of pitch and time.
+Convert midi to pitch and time arrays and reversely.
+
+## Requirements
+
+python >= 3.6
+
+Install requirements:
+
+```shell
+pip install mido
+pip install click
+```
+
+## Usage
+
+Convert midi file to array
+
+```shell
+python midi_to_array.py --input midi_file.mid --output file_to_store_array
+```
+
+Convert array to midi
+
+```shell
+python midi_to_array.py --input file_to_store_array --output output_midi.mid
+```
+
+## Output format
+
+Line 1: pitch array. R stands for rest.
+
+```
+R R C3 R G#2 R G#2 R G2 R G2 R ...
+```
+
+Line 2: duration array. The numbers are lengths relative to a whole note.
+
+```
+1.0104166666666667 16.21875 0.24479166666666666 0.005208333333333333 0.24479166666666666 0.005208333333333333 ...
+```
+
+If ``--format integer`` is set, pitches are represented by integers, where 60 stands for C4.
+
---
+++ midi_to_array.py
@@ -0,0 +1,209 @@
+from mido import MidiFile, MidiTrack, Message
+import click
+
+class MusicMapping:
+    '''
+    Provide two mappings: name to int, int to name
+    '''
+    name_to_int = {}
+    names = 'CDEFGAB'
+    offsets = [0, 2, 4, 5, 7, 9, 11]
+    start = 12
+    base = 12
+    for level in range(8):
+        for n, offset in zip(names, offsets):
+            name_to_int[n + str(level)] = start + level * base + offset
+            name_to_int[n + '#' + str(level)] = name_to_int[n + str(level)] + 1
+            name_to_int[n + 'b' + str(level)] = name_to_int[n + str(level)] - 1
+    name_to_int['C8'] = 108
+    name_to_int['R'] = -1
+
+    int_to_name = {}
+    for name, integer in name_to_int.items():
+        if '#' not in name and 'b' not in name:
+            int_to_name[integer] = name
+    for name, integer in name_to_int.items():
+        if 'b' not in name and int_to_name.get(integer) == None:
+            int_to_name[integer] = name
+    name_to_int[-1] = 'R'
+class Note:
+    def __init__(self, pitch, start_time) -> None:
+        self.pitch = pitch
+        self.start_time = start_time
+
+    def __eq__(self, __o: object) -> bool:
+        return self.pitch == __o.pitch
+
+    def __lt__(self, other) -> bool:
+        return self.pitch < other.pitch
+
+    def get_rest(start_time):
+        return Note(-1, start_time)
+
+class Channel:
+    def __init__(self) -> None:
+        self.playing_notes = [Note.get_rest(0)]
+        self.pitch = []
+        self.duration = []
+
+        self.prev_time = 0
+        self.cur_time = 0
+        self.note_on_buffer = []    # buffer notes arrive in the same time
+        self.note_off_buffer = []
+
+    def __len__(self):
+        return len(self.pitch)
+
+    def flush(self):
+        '''
+        Flush buffer.
+        Log the highest note that is preempted / turned off.
+        '''
+        if self.playing_notes: # not empty
+            high_pitch = max(self.playing_notes)
+
+            if self.note_on_buffer:
+                high_on = max(self.note_on_buffer)
+            else:
+                high_on = Note.get_rest(self.cur_time)
+
+            if high_pitch < high_on or high_pitch in self.note_off_buffer:
+                self.log_note(high_pitch)
+
+        for note in self.note_off_buffer:
+            self.playing_notes.remove(note)
+        self.playing_notes += self.note_on_buffer
+        self.note_on_buffer = []
+        self.note_off_buffer = []
+
+    def note_on(self, pitch, time):
+        '''
+        Turn on note [pitch] at global time [time].
+        '''
+        if time > self.cur_time:
+            self.flush()
+            self.cur_time = time
+        self.note_on_buffer.append(Note(pitch, time))
+
+    def note_off(self, pitch, time):
+        if time > self.cur_time:
+            self.flush()
+            self.cur_time = time
+        self.note_off_buffer.append(Note(pitch, time))
+
+    def mute_all(self, time):
+        self.flush()
+        self.cur_time = time
+            
+        if self.playing_notes:
+            high_pitch = max(self.playing_notes)
+            self.log_note(high_pitch)
+            self.playing_notes = [Note.get_rest(0)]
+
+    def log_note(self, note: Note):
+        if self.cur_time == self.prev_time:
+            return
+        
+        self.pitch.append(note.pitch)
+        self.duration.append(self.cur_time - self.prev_time)
+        self.prev_time = self.cur_time
+    
+
+def midi_2_array(input, output, format):
+    mid = MidiFile(input)
+    ticks_per_whole_note = mid.ticks_per_beat * 4
+    # ticks_per_whole_note = 1
+
+    for i, track in enumerate(mid.tracks):
+        channels = [Channel() for k in range(16)]
+        global_time = 0
+
+        for msg in track:
+            global_time += msg.time
+
+            if not msg.is_meta:
+                if msg.type == 'note_on' and msg.velocity > 0:
+                    cid = msg.channel
+                    channels[cid].note_on(msg.note, global_time)
+                elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
+                    cid = msg.channel
+                    channels[cid].note_off(msg.note, global_time)
+                elif msg.type == 'reset':
+                    for channel in channels:
+                        channel.mute_all(global_time)
+                elif msg.type == 'control_change' and msg.control == 123:
+                    cid = msg.channel
+                    channels[cid].mute_all(global_time)
+
+        for j, channel in enumerate(channels):
+            channel.flush()
+            if len(channel) > 1:
+                output_array(channel, output, f'-track-{i}-channel{j}', format, ticks_per_whole_note)
+    
+def output_array(channel: Channel, dst, suffix, format, ticks_per_whole_note):
+    result = ''
+    if format == 'integer':
+        result += ' '.join([str(p) for p in channel.pitch])
+    elif format == 'name':
+        result += ' '.join([MusicMapping.int_to_name[p] for p in channel.pitch])
+    result += '\n'
+    result += ' '.join([str(d / ticks_per_whole_note) for d in channel.duration])
+    result += '\n'
+    if dst == None:
+        print('------ ' + suffix)
+        print(result)
+    else:
+        with open(dst + suffix, 'w', encoding='utf-8') as fout:
+            fout.write(result)
+
+def process_array_input(pitches, duration, format, ticks_per_whole_note):
+    '''
+    Caution: the start_time field in Note now stands for duration
+    '''
+    if format == 'integer':
+        pitches = [int(p) for p in pitches]
+    elif format == 'name':
+        pitches = [MusicMapping.name_to_int[p] for p in pitches]
+    duration = [round(float(d) * ticks_per_whole_note) for d in duration]
+    return [Note(p, t) for p, t in zip(pitches, duration)]
+
+def array_to_midi(input, output, format):
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+    ticks_per_whole_note = mid.ticks_per_beat * 4
+    # ticks_per_whole_note = 1
+
+    with open(input, 'r') as fin:
+        pitches = fin.readline().split()
+        duration = fin.readline().split()
+        notes = process_array_input(pitches, duration, format, ticks_per_whole_note)
+
+    rest_time = 0
+    for note in notes:
+        if (note.pitch == -1):
+            rest_time += note.start_time
+        else:
+            track.append(Message(
+                'note_on', note=note.pitch, velocity=64, time=rest_time
+            ))
+            track.append(Message(
+                'note_off', note=note.pitch, velocity=0, time=note.start_time
+            ))
+            rest_time = 0
+
+    mid.save(output)
+
+@click.command()
+@click.option('--input', help='input file')
+@click.option('--output', default=None, help='output file, console by default')
+@click.option('--format', type=click.Choice(['name', 'integer'], case_sensitive=False),
+                help="pitch format", default='name')
+def convert(input, output, format):
+    if input.endswith('.mid') or input.endswith('.midi') or input.endswith('.MID') or input.endswith('.MIDI'):
+        midi_2_array(input, output, format)
+    else:
+        array_to_midi(input, output, format)
+
+if __name__ == '__main__':
+    convert()
